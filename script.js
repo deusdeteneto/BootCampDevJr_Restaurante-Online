5fff93b55e9cae5fa0b1e047cbe22034fd1ea156
Torres-Neto <torresneto.tn@gmail.com>
2024-07-31 18:27:27 -0300

    closes #1 - Migrar o projeto Menu para o Bootstrap

---
+++ script.js
@@ -0,0 +1,9 @@
+// Lista dos Pratos
+var prods = [
+  { id: 1, nome: "Bife com batata", preco: 30.0 },
+  { id: 2, nome: "Coxa de Frango Crocante", preco: 25.0 },
+  { id: 3, nome: "Carne de Panela", preco: 22.0 },
+  { id: 4, nome: "Farofa", preco: 10.0 },
+  { id: 5, nome: "Salada", preco: 8.0 },
+  { id: 6, nome: "Torresmo", preco: 12.0 },
+];
